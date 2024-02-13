810643c798b92b8a5d74f76055736d46b9f03315
Muhammad Syazwan Bin Ahmad <wan5xp@gmail.com>
2024-02-13 11:11:33 +0100

    Remove make step

--- finder-app/finder-test.sh
+++ finder-app/finder-test.sh
@@ -10,9 +10,6 @@ WRITESTR=AELD_IS_FUN
 WRITEDIR=/tmp/aeld-data
 username=$(cat conf/username.txt)
 
-make clean
-make writer
-
 if [ $# -lt 3 ]
 then
 	echo "Using default value ${WRITESTR} for string to write"
