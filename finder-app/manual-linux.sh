44e1df8d2135ee5e552782cf92906938cf3a3f31
Muhammad Syazwan Bin Ahmad <wan5xp@gmail.com>
2024-02-14 12:07:13 +0100

    Implement manual-linux for assignment 3 part 2

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -35,9 +35,20 @@ if [ ! -e ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ]; then
     git checkout ${KERNEL_VERSION}
 
     # TODO: Add your kernel build steps here
+    echo "Deep clean the build"
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} mrproper
+    echo "Build defconfig"
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} defconfig 
+    echo "Build vmlinux"
+    make -j4 ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} all
+    echo "Build modules"
+    make -j4 ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} modules
+    echo "Build devicetree"
+    make -j4 ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} dtbs
 fi
 
 echo "Adding the Image in outdir"
+cp "${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image" "${OUTDIR}/Image" 
 
 echo "Creating the staging directory for the root filesystem"
 cd "$OUTDIR"
@@ -48,6 +59,11 @@ then
 fi
 
 # TODO: Create necessary base directories
+mkdir rootfs
+cd rootfs
+mkdir -p bin dev etc home lib lib64 proc sbin sys tmp usr var
+mkdir -p usr/bin usr/lib usr/sbin
+mkdir -p var/log
 
 cd "$OUTDIR"
 if [ ! -d "${OUTDIR}/busybox" ]
@@ -61,20 +77,53 @@ else
 fi
 
 # TODO: Make and install busybox
+echo "Clean busybox"
+make distclean
+echo "Build defconfig"
+make defconfig
+echo "Build busybox"
+make -j4 ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE}
+echo "Install busybox to rootfs"
+make CONFIG_PREFIX=${OUTDIR}/rootfs ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} install
 
+cd "$OUTDIR/rootfs"
 echo "Library dependencies"
 ${CROSS_COMPILE}readelf -a bin/busybox | grep "program interpreter"
 ${CROSS_COMPILE}readelf -a bin/busybox | grep "Shared library"
 
 # TODO: Add library dependencies to rootfs
+cd $(dirname $(which ${CROSS_COMPILE}gcc))
+cd ../aarch64-none-linux-gnu/libc
+cp lib64/ld-2.31.so ${OUTDIR}/rootfs/lib64/
+cp lib64/libm.so.6 ${OUTDIR}/rootfs/lib64/
+cp lib64/libresolv.so.2 ${OUTDIR}/rootfs/lib64/
+cp lib64/libc.so.6 ${OUTDIR}/rootfs/lib64/
+cd ${OUTDIR}/rootfs/lib
+ln -s ../lib64/ld-2.31.so ld-linux-aarch64.so.1
 
 # TODO: Make device nodes
+cd ${OUTDIR}/rootfs
+sudo mknod -m 666 dev/null c 1 3
+sudo mknod -m 620 dev/console c 5 1
 
 # TODO: Clean and build the writer utility
-
+cd $FINDER_APP_DIR
+make clean
+make CROSS_COMPILE=${CROSS_COMPILE} -j4 writer
 # TODO: Copy the finder related scripts and executables to the /home directory
 # on the target rootfs
+cp writer ${OUTDIR}/rootfs/home/
+cp finder.sh ${OUTDIR}/rootfs/home/
+cp finder-test.sh ${OUTDIR}/rootfs/home/
+# Modify the finder-test.sh to reference conf/assignment
+sed -i 's/\.\.\/conf/conf/g' ${OUTDIR}/rootfs/home/finder-test.sh
+cp -r ../conf ${OUTDIR}/rootfs/home/conf
+cp autorun-qemu.sh ${OUTDIR}/rootfs/home/
 
 # TODO: Chown the root directory
+cd ${OUTDIR}/rootfs
+find . | cpio -H newc -ov --owner root:root > ${OUTDIR}/initramfs.cpio
 
 # TODO: Create initramfs.cpio.gz
+cd $OUTDIR
+gzip -f initramfs.cpio
